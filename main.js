97914532e7c5c8b5146f6c3a3f1eea94a69175b1
Кірюша <mkiril087@gmail.com>
2025-11-22 14:25:12 +0200

    gh

--- main.js
+++ main.js
@@ -1,6 +1,27 @@
-const imgDog = document.querySelector('.doggy');
-const inpRangeWidth = document.querySelector('.inpRangeWidth');
+const imgDog = document.querySelector(".doggy");
+const inpRangeWidth = document.querySelector(".inpRangeWidth");
 
-inpRangeWidth.addEventListener('input', _.debounce(() => {
+inpRangeWidth.addEventListener(
+  "input",
+  _.debounce(() => {
     imgDog.style.width = `${inpRangeWidth.value * 4.2}px`;
-}, 300),)
+  }, 300)
+);
+
+const box = document.querySelector(".box");
+
+// document.body.addEventListener("mousemove", (event) => {
+//   //   console.log(event.y);
+//   box.style.transform = `translate(-50%, -50%)`;
+//   box.style.top = `${event.y}px`;
+//   box.style.left = `${event.x}px`;
+// });
+
+document.body.addEventListener(
+  "mousemove",
+  _.debounce((event) => {
+    box.style.transform = `translate(-50%, -50%)`;
+    box.style.top = `${event.y}px`;
+    box.style.left = `${event.x}px`;
+  }, 100)
+);
